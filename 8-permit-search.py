e320493ebdcd92b414353bb57433992ac6a9429e
Brett Tackaberry <brett@rebel.com>
2023-07-20 11:10:40 -0400

    Remove cx from search

--- 8-permit-search.py
+++ 8-permit-search.py
@@ -5,7 +5,9 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 api_key = config['default']['google_search_api_key']
+search_cx = config['default']['google_search_cx']
+
 
 term = '"MS0034436" npdes permit type:pdf'
 
-x = requests.get(f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx=4078f5ea249074bf8&q={term}')
+x = requests.get(f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_cx}&q={term}')
